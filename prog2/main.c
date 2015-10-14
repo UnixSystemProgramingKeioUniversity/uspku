b121e422a45e90ffbbaade898dcecca0e70b493d
Takashi Ikeuchi <comeandseeme2012@gmail.com>
2015-10-14 12:54:45 -0300

    finished making help_proc

--- prog2/main.c
+++ prog2/main.c
@@ -47,11 +47,40 @@ void help_proc(int num, char *name[]){
   printf("\tinitialize hash list and free list and make the initial ");
   printf("state shown in the figure 2.15. \n ");
   //buf
-  printf("init : \n");
-  printf("\tinitialize hash list and free list and make the initial ");
-  printf("state shown in the figure 2.15. \n ");
+  printf("buf[n ...] \n");
+  printf("\tIf there is no input value, ");
+  printf("display all the status of the buffers.\n");
+  printf("\tIf there is an input block number, ");
+  printf("display the status of the buffer at the block number.\n");
+  //hash
+  printf("hash[n ...] \n");
+  printf("\tIf there is no input value, ");
+  printf("display all the hash list.\n");
+  printf("\tIf there is an input hash key, ");
+  printf("display the hash list at the hash key given from user.\n");
+  //free
+  printf("free\n");
+  printf("\tDisplay free list\n");
+  //getblk 
+  printf("getblk n\n");
+  printf("\ttake the blkno from the user, execute getblk(n)\n");
+  //brelse  
+  printf("brelse n\n");
+  printf("\ttake the blkno from the user, execute brelse(bp), \n");
+  printf("where bp is the pointer to buffer header with blkno = n\n");  
+  //set
+  printf("set n stat [stat]\n");
+  printf("\tset the status of the buffer of blkno n to stat\n");
+  //reset
+  printf("reset n stat [stat]\n");
+  printf("\treset the status of the buffer of blkno n to stat\n");
+  //quit
+  printf("exit the software\n");
 }
-void init_proc(int, char *[]);
+
+void init_proc(int, char *[]){
+  
+}
 void buf_proc(int, char *[]);
 void hash_proc(int, char *[]);
 void free_proc(int, char *[]);
@@ -60,12 +89,6 @@ void brelse_proc(int, char *[]);
 void set_proc(int, char *[]);
 void reset_proc(int, char *[]);
 void quit_proc(int, char *[]);
-
-void PrintInit(){
-  printf("init : \n");
-  printf("\tinitialize hash list and free list and make the initial ");
-  printf("state shown in the figure 2.15. \n ");
-}
 
 void PrintState(buf *p){
   int state = p -> stat;
