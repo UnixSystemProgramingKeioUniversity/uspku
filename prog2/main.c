6719904defc1800b6a585dd61a7a1b808d791205
Takashi Ikeuchi <comeandseeme2012@gmail.com>
2015-10-14 23:11:43 -0300

    main almos finished

--- prog2/main.c
+++ prog2/main.c
@@ -4,6 +4,7 @@
 #include "dlist.h"
 #include "state.h"
 #include "getblk.h"
+#include <assert.h>
 
 void help_proc(int, char *[]);
 void init_proc(int, char *[]);
@@ -35,8 +36,21 @@ struct command_table cmd_tbl[]{
   {NULL, NULL}
 };
 
-int main(){
+int main(int argc, char *argv[]){
+  struct command_table *p;
+  int ac;
+  char *av[16];
 
+
+  for(p = cmd_tbl; p -> cmd; p++){
+    if(strcmp(av[0], p -> cmd) == 0){
+      (*p -> func)(ac, av);
+      break;
+    }
+  }
+  if(p -> cmd == NULL){
+    fprintf(stderr, "unknown cmmand: %s\n", av[0]);
+  }
 
 }
 
@@ -78,17 +92,200 @@ void help_proc(int num, char *name[]){
   printf("exit the software\n");
 }
 
-void init_proc(int, char *[]){
-  
+void init_proc(int , char *[]){
+  //init process
+  for(int i = 0; i < NHASH; i++){
+    h_head[i].hash_fp = NULL;
+    h_head[i].hash_bp = NULL;
+    h_head[i].stat = 0;
+    h_head[i].free_fp = NULL;
+    h_head[i].free_bp = NULL;
+    h_head[i].cache_data = NULL;
+    buf *p = malloc(sizeof(buf));
+    buf *q = malloc(sizeof(buf));
+    buf *r = malloc(sizeof(buf));
+    if(p == NULL | q == NULL | r == NULL){
+      printf("unable to assign memory\n");
+      abort();
+    }
+    ASSERT(p != NULL && q != NULL && r != NULL);
+    insert_list(h_head[i], p, HASHHEAD);
+    insert_list(h_head[i].hash_fp, q, HASHHEAD);
+    insert_list(((h_head[i].hash_fp) -> hash_fp), r, HASHHEAD);
+  }
+  //adding buffers
+  //mod = 0
+  buf *trac = h_head[0].hash_fp;
+  trac -> blkno = 28;
+  AddStatus(trac, STAT_VALID);
+  trac = trac -> hash_fp;
+  trac -> blkno = 4;
+  AddStatus(trac, STAT_VALID);
+  trac = trac -> hash_fp;
+  trac -> blkno = 64;
+  AddStatus(trac, STAT_VALID | STAT_LOCKED);
+  //mod = 1
+  trac = h_head[1].hash_fp;
+  trac -> blkno = 17;
+  AddStatus(trac, STAT_VALID | STAT_LOCKED);
+  trac = trac -> hash_fp;
+  trac -> blkno = 5;
+  AddStatus(trac, STAT_VALID);
+  trac = trac -> hash_fp;
+  trac -> blkno = 97;
+  AddStatus(trac, STAT_VALID);
+  //mod = 2
+  trac = h_head[2].hash_fp;
+  trac -> blkno = 98;
+  AddStatus(trac, STAT_VALID | STAT_LOCKED);
+  trac = trac -> hash_fp;
+  trac -> blkno = 50;
+  AddStatus(trac, STAT_VALID | STAT_LOCKED);
+  trac = trac -> hash_fp;
+  trac -> blkno = 10;
+  AddStatus(trac, STAT_VALID);
+  //mod = 3
+  trac = h_head[3].hash_fp;
+  trac -> blkno = 3;
+  AddStatus(trac, STAT_VALID);
+  trac = trac -> hash_fp;
+  trac -> blkno = 35;
+  AddStatus(trac, STAT_VALID | STAT_LOCKED);
+  trac = trac -> hash_fp;
+  trac -> blkno = 99;
+  AddStatus(trac, STAT_VALID | STAT_LOCKED);
+
+  //adding freelist
+  trac = f_flist;
+  trac -> free_fp = NULL;
+  insert_list(trac, h_head[3].hash_fp, FREEHEAD);
+  trac = trac -> free_fp;
+  insert_list(trac, (h_head[1].hash_fp) -> hash_fp, FREEHEAD);
+  trac = trac -> free_fp;
+  insert_list(trac, (h_head[0].hash_fp) -> hash_fp, FREEHEAD);
+  trac = trac -> free_fp;
+  insert_list(trac, h_head[0].hash_fp, FREEHEAD);
+  trac = trac -> free_fp;
+  insert_list(trac, (h_head[1].hash_fp) -> hash_fp -> hash_fp, FREEHEAD);
+  trac = trac -> free_fp;
+  insert_list(trac, (h_head[2].hash_fp) -> hash_fp -> hash_fp, FREEHEAD);
+
 }
-void buf_proc(int, char *[]);
-void hash_proc(int, char *[]);
-void free_proc(int, char *[]);
-void getblk_proc(int, char *[]);
-void brelse_proc(int, char *[]);
-void set_proc(int, char *[]);
-void reset_proc(int, char *[]);
-void quit_proc(int, char *[]);
+void buf_proc(int argc, char *argv[]){
+  if(!argc){
+    PrintBufferAll();
+  }
+  else{
+    for(int i = 0; i < argc; i++){
+      if(isalnum((int)argv[i])){
+	PrintBufferOne((int)argv[i]);
+      }
+      else{
+	printf("INPUT VALUE MUST BE NUMBER, ASS WHOLE\n");
+	printf("NOTHING PRINTED FOR THIS SHITTY REQUEST\n");
+      }
+    }
+  }
+}
+void hash_proc(int argc, char *argv[]){
+  if(!argc){
+    PrintHashALL();
+  }
+  else{
+    for(int i = 0; i < argc; i++){
+      if(0 <= (int)argv[i] && (int)argv[i] < 4){
+	PrintHashLine((int)argv[i]);
+      }
+      else{
+	printf("INPUT VALUE MUST BE WITHIN 0~3, DUSHBAG\n");
+	printf("NOTHING PRINTED FOR THIS SHITTY REQUEST\n");
+      }
+    }
+  }
+}
+void free_proc(int, char *[]){
+  PrintFree();
+}
+void getblk_proc(int argc, char *argv[]){
+  if(!argc){
+    printf("COMMON MAN, WHICH BLOCK ARE YOU TALKING ABOUT\n");
+  }
+  else{
+    if(isalnum((int)argv[0])){
+      if((int)argv[0] <= 0 && (int)argv[0] < 12){
+	getblk((int)argv[0]);
+      }
+      else{
+	printf("INPUT VALUE MUST BE WITHING 0~11\n");
+      }
+    }
+    else{
+      printf("INPUT VALUE MUST BE NUMBER, ASS WHOLE\n");
+    }
+  }
+}
+void brelse_proc(int, char *[]){
+  if(!argc){
+    printf("COMMON MAN, WHICH BLOCK ARE YOU TALKING ABOUT\n");
+  }
+  else{
+    if(isalnum((int)argv[0])){
+      if((int)argv[0] <= 0 && (int)argv[0] < 12){
+	brelse((int)argv[0]);
+      }
+      else{
+	printf("INPUT VALUE MUST BE WITHING 0~11\n");
+      }
+    }
+    else{
+      printf("INPUT VALUE MUST BE NUMBER, ASS WHOLE\n");
+    }
+  }
+}
+void set_proc(int argc, char *argv[]){
+  if(!argc){
+    printf("JUST DON'T DO THE SHITS\n");
+  }
+  else{
+    if(isalnum((int)argv[0])){
+      if((int)argv[0] <= 0 && (int)argv[0] < 12){
+	int stat = 0;
+	for(int i = 1; i < argc; i++){
+	  stat += (int)argv[i];
+	}
+	buf *p = GetBuf(h_head,(int)argv[0]); 
+	SetStatus(p, stat);
+      }
+      else{
+	printf("INPUT VALUE MUST BE WITHING 0~11\n");
+      }
+    }
+  }
+}
+
+void reset_proc(int, char *[]){
+  if(!argc){
+    printf("JUST DON'T DO THE SHITS\n");
+  }
+  else{
+    if(isalnum((int)argv[0])){
+      if((int)argv[0] <= 0 && (int)argv[0] < 12){
+	int stat = 0;
+	for(int i = 1; i < argc; i++){
+	  stat += (int)argv[i];
+	}
+	buf *p = GetBuf(h_head,(int)argv[0]); 
+	ResetStatus(p, stat);
+      }
+      else{
+	printf("INPUT VALUE MUST BE WITHING 0~11\n");
+      }
+    }
+  }
+}
+void quit_proc(int, char *[]){
+  return;  
+}
 
 void PrintState(buf *p){
   int state = p -> stat;
@@ -184,4 +381,34 @@ int SearchNum(int blkno){
       index++; 
     }    
   }
+}
+
+buf *init(){
+  buf *p = malloc(sizeof(buf));
+  p -> hash_fp = NULL;
+  p -> hash_bp = NULL;
+  p -> stat = 0;
+  p -> free_fp = NULL;
+  p -> free_bp = NULL;
+  p -> cache_data = NULL;
+
+  return p;
+}
+
+void SetStatus(buf *h, int stat){
+  h -> stat = stat;
+}
+
+void ResetStatus(buf *h, int stat){
+  h -> stat ^ stat;
+}
+
+buf *GetBuf(int index){
+  int quotient = index / 4;
+  int remainder = index % 4;
+  buf *p = h_head[quotient].hash_fp;
+  for(int i = 0; i < remainder; i++){
+    p -> hash_fp;
+  }
+  return p;
 }
